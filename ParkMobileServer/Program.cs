da53d65eb1b3f3c3f1bd049a4db58673c2155307
Kiruha <93467283+eviolspirid777@users.noreply.github.com>
2024-12-24 18:01:20 +0300

    Фикс тодо

--- ParkMobileServer/Program.cs
+++ ParkMobileServer/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.IdentityModel.Tokens;
 using ParkMobileServer.DbContext;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.ResponseCompression;
 
 /*
  * Команды для развертывания докера на серваке
@@ -41,6 +42,29 @@ namespace ParkMobileServer
 
 			builder.Services.AddControllers();
 
+			//TODO: Можно сжатие добавить для более лучшей оптимизации
+			//TODO: Можно хранить изображения для слайдера в wwwroot для более быстрой загрузки
+			// // Тут нужно реализовать сжатие данных через gzip, deflate или br для оптимизации запросов.
+			// // Никита предложил прибегнуть к потоковой передачи, но это как-то необычно и странно и бот говорит, что не рационально в этом случае
+			// // Add response compression services
+			// builder.Services.AddResponseCompression(options =>
+			// {
+			// 	options.EnableForHttps = true;
+			// 	options.Providers.Add<GzipCompressionProvider>();
+			// 	options.Providers.Add<BrotliCompressionProvider>();
+			// });
+
+			// // Configure compression options
+			// builder.Services.Configure<BrotliCompressionProviderOptions>(options =>
+			// {
+			// 	options.Level = System.IO.Compression.CompressionLevel.Fastest;
+			// });
+
+			// builder.Services.Configure<GzipCompressionProviderOptions>(options =>
+			// {
+			// 	options.Level = System.IO.Compression.CompressionLevel.Fastest;
+			// });
+
 			string secretKey = builder.Configuration["JwtSecret"]; // �������� JwtSecret � appsettings.json ��� ���������� ��������� 
 			if (string.IsNullOrEmpty(secretKey))
 			{
@@ -77,6 +101,9 @@ namespace ParkMobileServer
 
 			app.UseAuthentication();
 			app.UseAuthorization();
+
+			// Use response compression middleware
+			// app.UseResponseCompression();
 
 			app.MapControllers();
 
