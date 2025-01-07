5366032541baff16994ae54c5d27ac2f5fac3192
eviolspirid <eviolspirid@gmail.com>
2025-01-07 21:28:06 +0300

    Исправлен баг с редиректом запросов

--- ParkMobileServer/Program.cs
+++ ParkMobileServer/Program.cs
@@ -26,7 +26,6 @@ namespace ParkMobileServer
 		{
 			var builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
 			builder.Services.AddDbContext<PostgreSQLDbContext>(options =>
 			{
 				options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
@@ -40,33 +39,16 @@ namespace ParkMobileServer
 
 			builder.WebHost.UseUrls("https://*:3001");
 
-			builder.Services.AddControllers();
+            //builder.Services.AddHttpsRedirection(options =>
+            //{
+            //    options.HttpsPort = 5001;
+            //});
+
+            builder.Services.AddControllers();
 
 			//TODO: Redis можно прикрутить и не париться(возможно это и зафиксит баг с оптимизацией слайдера)
-			//TODO: Можно сжатие добавить для более лучшей оптимизации
-			//TODO: Можно хранить изображения для слайдера в wwwroot для более быстрой загрузки
-			// // Тут нужно реализовать сжатие данных через gzip, deflate или br для оптимизации запросов.
-			// // Никита предложил прибегнуть к потоковой передачи, но это как-то необычно и странно и бот говорит, что не рационально в этом случае
-			// // Add response compression services
-			// builder.Services.AddResponseCompression(options =>
-			// {
-			// 	options.EnableForHttps = true;
-			// 	options.Providers.Add<GzipCompressionProvider>();
-			// 	options.Providers.Add<BrotliCompressionProvider>();
-			// });
 
-			// // Configure compression options
-			// builder.Services.Configure<BrotliCompressionProviderOptions>(options =>
-			// {
-			// 	options.Level = System.IO.Compression.CompressionLevel.Fastest;
-			// });
-
-			// builder.Services.Configure<GzipCompressionProviderOptions>(options =>
-			// {
-			// 	options.Level = System.IO.Compression.CompressionLevel.Fastest;
-			// });
-
-			string secretKey = builder.Configuration["JwtSecret"]; // �������� JwtSecret � appsettings.json ��� ���������� ��������� 
+			string secretKey = builder.Configuration["JwtSecret"];
 			if (string.IsNullOrEmpty(secretKey))
 			{
 				throw new Exception("JwtSecret �� �����!");
@@ -98,13 +80,10 @@ namespace ParkMobileServer
 						.AllowAnyMethod()
 						.AllowAnyHeader());
 
-			// Configure the HTTP request pipeline.
+			app.UseHttpsRedirection();
 
 			app.UseAuthentication();
 			app.UseAuthorization();
-
-			// Use response compression middleware
-			// app.UseResponseCompression();
 
 			app.MapControllers();
 
