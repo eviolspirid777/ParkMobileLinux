36cbabfa33e3739bd1204de2bd2908cd717ecffd
eviolspirid <eviolspirid@gmail.com>
2025-01-11 12:54:56 +0300

    Добавлена яндекс.метрика

--- ParkMobileServer/park-mobile-ssr/src/app/layout.tsx
+++ ParkMobileServer/park-mobile-ssr/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./App.scss";
-import Head from "next/head";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Park Mobile",
@@ -16,23 +16,24 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <Head>
-        <script
-          type="text/javascript"
-          src="https://mc.yandex.ru/metrika/tag.js"
-          async
-        ></script>
-        <noscript>
-          <div>
-            <img
-              src="https://mc.yandex.ru/watch/99483189"
-              style={{ position: 'absolute', left: '-9999px' }}
-              alt=""
-            />
-          </div>
-        </noscript>
-      </Head>
-      <body>{children}</body>
+      <body>
+        <Script id="yandex-metrica" strategy={"afterInteractive"}>
+          {`
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+            ym(99483189, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true
+            });
+          `}
+        </Script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/99483189" style={{position:"absolute", left:"-9999px"}} alt="" /></div></noscript>
+        {children}
+      </body>
     </html>
   );
 }
